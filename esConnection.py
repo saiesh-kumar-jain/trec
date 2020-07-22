34d7165dc4f2cb309b68d45a4c59ed3fd950fc2a
saiesh-devp <jain.sai@northeastern.edu>
2020-07-22 17:01:54 -0400

    Lands on main page

--- esConnection.py
+++ esConnection.py
@@ -11,24 +11,29 @@ es = Elasticsearch('https://35a530cd680844118e0642de8fc07b63.us-central1.gcp.clo
 query = "lampedusa migrant shipwreck"
 topDocs = []
 scoredDocs = []
+name = "saiesh"
+queryId = "1"
 
 app = Flask(__name__)
 
 #Default
-@app.route("/")
+@app.route("/", methods = ["POST", "GET"])
 def home():
-    return render_template("index.html")
-
-@app.route("/evaluateDocs", methods = ["POST", "GET"])
-def evaluateDocs():
-    user = request.form["txtName"]
-    print(user)
     with open("./qreldata/qrelsaiesh.txt", "r") as file:
         for eachLine in file:
             data = eachLine.split()
             scoredDocs.append(data[2])
     file.close()
-    return render_template("assesment.html", docs = topDocs, query = query, scoredDocs = scoredDocs)
+    return render_template("assesment.html", docs=topDocs, query=query, scoredDocs=scoredDocs)
+
+# @app.route("/evaluateDocs", methods = ["POST", "GET"])
+# def evaluateDocs():
+#     with open("./qreldata/qrelsaiesh.txt", "r") as file:
+#         for eachLine in file:
+#             data = eachLine.split()
+#             scoredDocs.append(data[2])
+#     file.close()
+#     return render_template("assesment.html", docs = topDocs, query = query, scoredDocs = scoredDocs)
 
 @app.route("/score", methods = ["GET"])
 def scoreDocument():
@@ -38,7 +43,7 @@ def scoreDocument():
     score = request.args.get("score")
     print(score)
     with open("./qreldata/qrelsaiesh.txt","a") as file:
-        l = "\n" +"1 "+"saiesh " + docId + " " + str(score)
+        l = "\n" + queryId + " " + name +" "+ docId + " " + str(score)
         file.write(l)
     file.close()
 
